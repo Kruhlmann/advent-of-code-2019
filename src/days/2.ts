fed08cd0868565b57f0ed85730ab3903b761ff46
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-02 19:02:46 +0000

    Day 2 task 2 complete

--- src/days/2.ts
+++ src/days/2.ts
@@ -18,11 +18,35 @@ get_puzzle_input(2).then((input_str_arr: string[]) => {
     const input = input_str_arr[0].split(",").map((i) => parseInt(i));
 
     // Task 1.
-    input[1] = 12;
-    input[2] = 2;
-    const t_1_res = run_program(input);
-    console.log(`\t Task 1: ${t_1_res}`)
+    const program = input.slice();
+    program[1] = 12;
+    program[2] = 2;
+    const t_1_res = run_program(program);
+    console.log(`\tTask 1: ${t_1_res}`);
 
     // Task 2.
+    const target = 19690720;
+    let found = false;
+
+    for (let noun = 0; noun <= 99; noun ++) {
+        for (let verb = 0; verb <= 99; verb ++) {
+            const program = input.slice();
+            program[1] = noun;
+            program[2] = verb;
+            const res = run_program(program);
+            if (res === target) {
+                found = true;
+                console.log(`\tTask 2: ${100 * noun + verb}`)
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+
+    if (!found) {
+        console.log("\tTask 2: Not found!")
+    }
 });
 
