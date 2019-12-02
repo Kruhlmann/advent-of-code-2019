e9cb040f7ef922966be43133e5f0f296c4cf24f4
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-02 01:05:42 +0000

    Day one task 1 done

--- src/days/1.ts
+++ src/days/1.ts
@@ -1,3 +1,18 @@
 import { get_puzzle_input } from "../lib";
 
-console.log(1)
+// Fuel required to launch a given module is based on its mass. Specifically,
+// to find the fuel required for a module, take its mass, divide by three,
+// round down, and subtract 2.
+function mass_fuel_req(mass) {
+    return Math.floor(mass / 3) - 2;
+}
+
+get_puzzle_input(1).then((input: string[]) => {
+    let total_fuel_req = 0;
+    for (const mass_str of input) {
+        const mass_num = parseInt(mass_str);
+        total_fuel_req += mass_fuel_req(mass_num);
+    }
+    console.log(`\tTask 1 answer: ${total_fuel_req}`);
+});
+
--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,24 @@
-import * as fs from "fs"
-import * as request from "request"
+import * as fs from "fs";
+import * as request from "request";
+import * as path from "path";
 
-export async function get_puzzle_input (day: string): Promise<string> {
+export async function get_puzzle_input (day: string | number): Promise<any> {
     return new Promise((resolve, reject) => {
-        const cookie_file = fs.readFileSync("../cookie.secret", "utf8");
+        const cookie_path = path.resolve(__dirname, "../cookie.secret");
+        const cookie_file = fs.readFileSync(cookie_path, "utf8");
         const cookie = request.cookie(`session=${cookie_file}`);
         const cookie_jar = request.jar();
         const url = `https://adventofcode.com/2019/day/${day}/input`;
         cookie_jar.setCookie(cookie, url);
-        request({url: url, jar: cookie_jar}, (error, response, body) => error ? reject(error) : resolve(body.split(/\n/g)));
-    }).catch(error => console.error(error));
+        request({
+            url: url,
+            jar: cookie_jar
+        }, (error, response, body) => {
+            if (error) {
+                reject("error");
+            }
+            resolve(body.split(/\n/g).filter((l) => l && l !== ""));
+        });
+    }).catch((error) => console.error(error));
 }
 
