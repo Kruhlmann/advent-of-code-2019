009e1e6807e51baf016630c97362ff5f1092ccc3
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-02 01:20:26 +0000

    Day one task 2 done

--- src/days/1.ts
+++ src/days/1.ts
@@ -4,20 +4,21 @@ import { get_puzzle_input } from "../lib";
 // to find the fuel required for a module, take its mass, divide by three,
 // round down, and subtract 2.
 function mass_fuel_req(mass) {
-    return Math.floor(mass / 3) - 2;
+    return Math.max(0, Math.floor(mass / 3) - 2);
 }
 
 function mass_fuel_req_recursive(mass) {
     let total_fuel_req = 0;
-    let current_mass = mass;
-    let tmp_fuel_req = mass_fuel_req(mass);
-    total_fuel_req += tmp_fuel_req;
-    while (tmp_fuel_req > 0) {
-        current_mass = tmp_fuel_req;
-        tmp_fuel_req = mass_fuel_req(mass);
-        total_fuel_req += tmp_fuel_req;
-    };
+    let fuel_req;
+
+    while (fuel_req === undefined || fuel_req > 0) {
+        fuel_req = mass_fuel_req(mass);
+        mass = fuel_req;
+        total_fuel_req += fuel_req;
+    }
     return total_fuel_req;
+
+
 }
 
 get_puzzle_input(1).then((input: string[]) => {
@@ -30,9 +31,6 @@ get_puzzle_input(1).then((input: string[]) => {
     console.log(`\tTask 1 answer: ${total_fuel_req}`);
 
     // Task 2.
-
-    console.log(mass_fuel_req_recursive(1969))
-    return;
     total_fuel_req = 0;
     for (const mass_str of input) {
         const mass_num = parseInt(mass_str);
