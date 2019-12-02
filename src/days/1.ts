a07320ef5723f73316c5272dcfb5f19cffc2c8d4
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-02 01:13:21 +0000

    Day one task 1 done

--- src/days/1.ts
+++ src/days/1.ts
@@ -7,11 +7,36 @@ function mass_fuel_req(mass) {
     return Math.floor(mass / 3) - 2;
 }
 
+function mass_fuel_req_recursive(mass) {
+    let total_fuel_req = 0;
+    let current_mass = mass;
+    let tmp_fuel_req = mass_fuel_req(mass);
+    total_fuel_req += tmp_fuel_req;
+    while (tmp_fuel_req > 0) {
+        current_mass = tmp_fuel_req;
+        tmp_fuel_req = mass_fuel_req(mass);
+        total_fuel_req += tmp_fuel_req;
+    };
+    return total_fuel_req;
+}
+
 get_puzzle_input(1).then((input: string[]) => {
+    // Task 1.
     let total_fuel_req = 0;
     for (const mass_str of input) {
         const mass_num = parseInt(mass_str);
         total_fuel_req += mass_fuel_req(mass_num);
+    }
+    console.log(`\tTask 1 answer: ${total_fuel_req}`);
+
+    // Task 2.
+
+    console.log(mass_fuel_req_recursive(1969))
+    return;
+    total_fuel_req = 0;
+    for (const mass_str of input) {
+        const mass_num = parseInt(mass_str);
+        total_fuel_req += mass_fuel_req_recursive(mass_num);
     }
     console.log(`\tTask 1 answer: ${total_fuel_req}`);
 });
