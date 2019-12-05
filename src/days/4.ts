e88f70a8409bbab9e1f6b128cccfe3733fcc9f75
Andreas Krühlmann <kruhlmann@protonmail.com>
2019-12-05 17:51:21 +0100

    Day 4 task 1 complete

---
+++ src/days/4.ts
@@ -0,0 +1,27 @@
+import { get_puzzle_input } from "../lib";
+
+get_puzzle_input(4).then((input_str_arr: string[]) => {
+    const password_range = input_str_arr[0].split("-").map((i) => parseInt(i));
+    const password_pool = [];
+    for (let i = password_range[0]; i <= password_range[1]; i++) {
+        password_pool.push(i);
+    }
+
+    let valid_pwds = 0;
+    for (const _candidate of password_pool) {
+        const candidate = `${_candidate}`;
+        let valid = true;
+        valid = valid && (`${candidate}` === candidate.split("").sort().join(""));
+        valid = valid && (new Set(candidate.split("")).size !== candidate.length);
+        valid = valid && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((r, i) => r + (candidate.includes(`${i}${i}`) ? 1 : 0)) > 0
+
+        if (valid) {
+            valid_pwds ++;
+        }
+    }
+
+    console.log(`\tTask 1: ${valid_pwds}`)
+
+    console.log(`\tTask 2: None`);
+});
+
