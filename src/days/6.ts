d5bfddc805198a96e6621b0f6c6b51e75b8e58b1
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-06 07:00:19 +0000

    Day 6 task 1 complete

---
+++ src/days/6.ts
@@ -0,0 +1,40 @@
+import { get_puzzle_input } from "../lib";
+
+get_puzzle_input(6).then((input_str_arr: string[]) => {
+    const obj = [];
+    const obj_parents = {};
+    const obj_conn = {};
+
+
+
+    for (const orbit of input_str_arr) {
+        const [a, b] = orbit.split(")");
+        obj.push(a);
+        obj.push(b);
+        obj_parents[b] = a;
+
+        if (!obj_conn.hasOwnProperty(a)) {
+            obj_conn[a] = [b];
+        } else {
+            obj_conn[a].push(b)
+        }
+
+        if (!obj_conn.hasOwnProperty(b)) {
+            obj_conn[b] = [a];
+        } else {
+            obj_conn[b].push(a)
+        }
+    }
+
+    let count = 0;
+    for (let o of obj.filter((_o,_i) => obj.indexOf(_o) === _i)) {
+        while (o !== "COM") {
+            o = obj_parents[o];
+            count ++;
+        }
+    }
+
+    console.log(`Task 1: ${count}`);
+
+});
+
