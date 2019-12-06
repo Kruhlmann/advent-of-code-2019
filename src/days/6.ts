9d095e44c589f48a195e1a9c529b3e7ffae1e3d2
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-06 07:06:34 +0000

    Day 6 task 2 complete

--- src/days/6.ts
+++ src/days/6.ts
@@ -5,8 +5,6 @@ get_puzzle_input(6).then((input_str_arr: string[]) => {
     const obj_parents = {};
     const obj_conn = {};
 
-
-
     for (const orbit of input_str_arr) {
         const [a, b] = orbit.split(")");
         obj.push(a);
@@ -35,6 +33,39 @@ get_puzzle_input(6).then((input_str_arr: string[]) => {
     }
 
     console.log(`Task 1: ${count}`);
+
+    let a = obj_parents["YOU"];
+    let b = obj_parents["SAN"];
+    let closest_int;
+    const possible_int = [];
+
+    while (a !== "COM") {
+        possible_int.push(a);
+        a = obj_parents[a]
+    }
+
+    while (b !== "COM") {
+        if (possible_int.includes(b)) {
+            closest_int = b;
+            break;
+        }
+        b = obj_parents[b];
+    }
+
+    count = 0;
+    a = obj_parents["YOU"];
+    b = obj_parents["SAN"];
+    while (a !== closest_int) {
+        count ++;
+        a = obj_parents[a];
+    }
+    while (b !== closest_int) {
+        count ++;
+        b = obj_parents[b];
+    }
+
+    console.log(`Task 2: ${count}`);
+
 
 });
 
